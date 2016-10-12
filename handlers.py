b531ba2c1bcca008a4cd0fd0cacfbcb6f7b5da4e
bluehood <blue@lcm.mi.infn.it>
2016-10-12 16:56:00 +0200

    use Message.reply_* calls instead of Bot.send*
    This is in line with the latest development of the python-telegram-bot
    package

--- handlers.py
+++ handlers.py
@@ -14,20 +14,20 @@ logging.basicConfig(format=fmt, level=logging.INFO)
 
 def start(bot, update):
     """ Start off with an anectode """
-    bot.sendMessage(chat_id=update.message.chat_id,
-                    text="Oh raga, l'altro giorno la vale mi fa 'oh raga'")
+    update.message.reply_text(text="Oh raga, l'altro giorno la vale mi fa "
+                                   "'oh raga'")
 
 
 def no_dai_Geeeeerry(bot, update):
     """ Rebut Gerry's argument vigorously """
     voice_file_id = 'AwADBAADCwADzPSlEYhD8dBXfH8LAg'
-    bot.sendVoice(chat_id=update.message.chat_id, voice=voice_file_id)
+    update.message.reply_voice(voice=voice_file_id)
 
 
 def talk(bot, update):
     """ Engage in meaningful conversation """
     text = update.message.text
-    chat_id = update.message.chat_id
+    msg = update.message
 
     # ad-hoc replies to interesting messages
     for bhv in behaviours:
@@ -36,9 +36,9 @@ def talk(bot, update):
                 rep_type = bhv['reply_type']
                 reply = np.random.choice(bhv['reply'])
                 if rep_type == 'text':
-                    bot.sendMessage(chat_id=chat_id, text=reply)
+                    msg.reply_text(text=reply)
                 elif rep_type == 'sticker':
-                    bot.sendSticker(chat_id=chat_id, sticker=reply)
+                    msg.reply_sticker(sticker=reply)
 
     # random replies to uninteresting messages
     if rand() < 6.9/420.:
@@ -46,19 +46,18 @@ def talk(bot, update):
         # bl4ckst0ne is completely paralysed while asking for BBQ confirmation
         seconds_before_nagging = 15
         sleep(seconds_before_nagging)
-        bot.sendMessage(chat_id=chat_id,
-                        text='oh raga ma la grigliata è confermata?')
+        msg.reply_text(text='oh raga ma la grigliata è confermata?')
     if rand() < 6.9/420.:
         vignati_hat = 'BQADBAADRAADnWzWBo9KlpThN0OQAg'
         vignati_bw = 'BQADBAADcQADnWzWBjwXcOqPvseKAg'
         stickers = [vignati_hat, vignati_bw]
-        bot.sendSticker(chat_id=chat_id, sticker=np.random.choice(stickers))
+        msg.reply_sticker(sticker=np.random.choice(stickers))
 
 
 def speak(bot, update, args):
     """ Say what's on your mind """
     word = args[0] if len(args) > 0 else None
-    bot.sendMessage(chat_id=update.message.chat_id, text=produce_sentence(word))
+    update.message.reply_text(text=produce_sentence(word))
 
 
 def print_msg_info(bot, update):
